c5a65cbd7a6747cdee86512b51be0cf09aba2a92
mdrkarimi20 <mdrkarimi20@gmail.com>
2020-09-04 15:40:36 +0430

    insert component NewList

---
+++ src/Components/NewList/NewList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import { connect } from "react-redux";
+
+import { checkTask } from "../../Redux/ToDo/ToDoActions";
+
+function List({ title, done, listId, taskId, checkTask }) {
+  const taskCheckHandler = () => {
+    checkTask(listId, taskId);
+  };
+
+  return (
+    <ListItem>
+      <ListItemIcon>
+        <Checkbox checked={done} onChange={taskCheckHandler} />
+      </ListItemIcon>
+      <ListItemText>{title}</ListItemText>
+    </ListItem>
+  );
+}
+export default connect(null, { checkTask })(List);
